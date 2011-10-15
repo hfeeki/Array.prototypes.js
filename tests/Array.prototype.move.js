69ad913da23b922620f5195fb50964c958e92c76
Chris O <chris@prettycode.org>
2011-10-15 08:02:39 +0000

    Added test for Array.prototype.move. Haven't run it yet or hooked into a runner.

---
+++ tests/Array.prototype.move.js
@@ -0,0 +1,97 @@
+(function(tests) {
+
+    tests.require("../src/Array.prototype.move.js");
+
+    /*
+
+    // inline test execution:
+
+    verify.test("", function(assert) {
+
+    });
+
+    // exporting tests and running suite
+
+    verify.add("name of unit being tested", [
+        {
+            verify: "test title",
+            test: function(assert) {
+
+            }
+        }
+    ]);
+
+    verify.run();
+
+    */
+
+    tests.add([
+        {
+            verify: "Throws when `this` is not an Array",
+            test: function() {
+                var foo;
+
+                assert(function() {
+                    Array.prototype.move.call(foo, 0);
+                }).throws(TypeError);
+
+                assert(function() {
+                    Array.prototype.move.call({}, 0);
+                }).throws(TypeError);
+
+                assert(function() {
+                    Array.prototype.move.call(true, 0);
+                }).throws(TypeError);
+
+                assert(function() {
+                    Array.prototype.move.call(49, 0);
+                }).throws(TypeError);
+
+                assert(function() {
+                    Array.prototype.move.call(null, 0);
+                }).throws(TypeError);
+
+                assert(function() {
+                    Array.prototype.move.call("string", 0);
+                }).throws(TypeError);
+
+                assert(function() {
+                    Array.prototype.move.call(function(){}, 0);
+                }).throws(TypeError);
+            }
+        },
+        {
+            verify: "Returns `this` Array it acts on",
+            test: function() {
+                var array = [1, "<", 2];
+                assert(array.move(0, 1)).is(array);
+            }
+        },
+        {
+            verify: "TODO: What happens when I do this? Does this pass? Should it throw when from === to?",
+            test: function() {
+                assert(["a"].move(0, 0)).defined();
+            }
+        },
+        {
+            verify: "Relocates element to new index",
+            test: function() {
+                assert(
+                    [
+                        "chris",
+                        "farah",
+                        "lola"
+                    ]
+                    .move(0, 2)
+                    .move(1, 0)
+                )
+                .identical([
+                    "lola",
+                    "farah",
+                    "chris"
+                ]);
+            }
+        }
+    ]);
+
+})(verify);
--- todo.md
+++ todo.md
@@ -4,5 +4,5 @@
 * Write Array.prototype.shuffle.js
 * Write tests using the github.com/prettycode/verify.js testing framework
 * Normalize quotes (some files use `'`, while others use `"`)
-* Create a file with all the prototypes combined
-* Create a minified version of said file
+* Create a file with all the prototypes combined *and* refactor into cohesive "library"
+* Create a minified version of combined file
