1c7a9e362181f49fa96172a5077a2806ee44bc36
Chris O. <accounts@prettycode.org>
2013-04-26 08:47:27 -0700

    Update Array.prototype.contains.js

--- src/Array.prototype.contains.js
+++ src/Array.prototype.contains.js
@@ -1,13 +1,5 @@
 // Returns a boolean indicating whether an array has any matching value to the first argument specified
 
-Array.prototype.contains = function() {
-
-    // Prototypes throw `TypeError`s when the context are invalid
-
-    if (Object.prototype.toString.call(this) !== '[object Array]') {
-        throw new TypeError("`this` must be Array, not " + typeof this);
-    }
-
-    return this.indexOf(this) !== -1;
-
+Array.prototype.contains = function(value) {
+    return Array.prototype.indexOf.call(this, value) !== -1;
 };
